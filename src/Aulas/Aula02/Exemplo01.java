aa0dc122f18b56a2751388fbc086a0ff8a816251
ARTHUR ARAUJO SOUSA <arthur.asousa@senacsp.edu.br>
2025-03-14 22:13:30 -0300

    Adicionado aula04

--- src/Aulas/Aula02/Exemplo01.java
+++ src/Aulas/Aula02/Exemplo01.java
@@ -6,7 +6,7 @@ public class Exemplo01 {
   public static void main(String[] args) {
     int[][] matriz = new int[3][3];
     Scanner sc = new Scanner(System.in);
-    System.out.println("Matriz M[3][3]\n");
+    System.out.println("Matriz [3][3]\n");
     for(int linha = 0; linha < 3; linha++){
       for(int coluna = 0; coluna < 3; coluna++){
         System.out.printf("Insira o elemento [%d][%d]: ", linha+1, coluna+1);
